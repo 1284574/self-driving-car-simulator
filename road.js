8b1e5a260132da90896b7120c00cfd750f97dc90
Aaryaman Singh <63762732+1284574@users.noreply.github.com>
2023-07-28 12:27:27 -0400

    Update road.js 1:10:07

--- road.js
+++ road.js
@@ -26,35 +26,26 @@ class Road{
         ctx.lineWidth = 5;
         ctx.strokeStyle = "white";
 
-        for(let i = 0; i <= this.laneCount; i++)
-        {
-            const x = lerp(
+      for(let i=1;i<=this.laneCount-1;i++){
+            const x=lerp(
                 this.left,
                 this.right,
-                i / this.laneCount
+                i/this.laneCount
             );
-            if(i > 0 && i < this.laneCount)
-            {
-                ctx.setLineDash([20, 20]);
-            }
-            else
-            {
-                ctx.setLineDash([]);
-            }
-
-        ctx.beginPath();
-        ctx.moveTo(x, this.top);
-        ctx.lineTo(x, this.bottom);
-        ctx.stroke();
-
-       }
+            
+            ctx.setLineDash([20,20]);
+            ctx.beginPath();
+            ctx.moveTo(x,this.top);
+            ctx.lineTo(x,this.bottom);
+            ctx.stroke();
+        }
        ctx.setLineDash([]);
        this.borders.forEach(border => {
          ctx.beginPath();
          ctx.moveTo(border[0].x, border[0].y);
          ctx.lineTo(border[1].x, border[1].y);
          ctx.stroke();
-       })
+       });
    } 
 }
 
